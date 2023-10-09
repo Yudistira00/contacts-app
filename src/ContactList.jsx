2905f09b6ce868325a090ae7ff9042ab2ac3174b
yudistira <ict.programmer@tvip.co.id>
2023-10-09 21:11:05 +0700

    create ui contact apps

---
+++ src/ContactList.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import ContactItem from "./ContactItem";
+
+// function ContactList({contacts}) {
+//     return (
+//         <div className="contact-list">
+//             {
+//                  contacts.map((contact) => (
+//                     <ContactItem key={contact.id} {...contact}/>
+//                 ))
+//             }
+//         </div>
+//     );
+// }
+// export default ContactList;
+
+import React from 'react';
+import ContactItem from './ContactItem';
+ 
+function ContactList({ contacts }) {
+ return (
+   <div className="contact-list">
+     {
+       contacts.map((contact) => (
+         <ContactItem key={contact.id} {...contact} />
+       ))
+     }
+   </div>
+ );
+}
+ 
+export default ContactList;
